214fe0e031bcbf3d5e24e07be177bae3333c6bd8
MohamadRobatjazi <mohamadrobatjazi1387@gmail.com>
2026-08-03 21:34:03 +0330

    feat: install login-02 and complete LoginView

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,3 +1,31 @@
-<template></template>
+<template>
+    <div class="grid min-h-svh lg:grid-cols-2">
+        <div class="flex flex-col gap-4 p-6 md:p-10">
+            <div class="flex justify-center gap-2 md:justify-start">
+                <a href="#" class="flex items-center gap-2 font-medium">
+                    <div
+                        class="bg-primary text-primary-foreground flex h-6 w-6 items-center justify-center rounded-md"
+                    ></div>
+                    Acme Inc.
+                </a>
+            </div>
+            <div class="flex flex-1 items-center justify-center">
+                <div class="w-full max-w-xs">
+                    <LoginForm />
+                </div>
+            </div>
+        </div>
 
-<script setup lang="ts"></script>
+        <div class="bg-muted relative hidden lg:block">
+            <img
+                src=""
+                alt="Image"
+                class="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import LoginForm from '@/components/auth/LoginForm.vue';
+</script>
