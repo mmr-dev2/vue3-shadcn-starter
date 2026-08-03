b278cac646521048127b3788d4e0b7d581e5eb02
MohamadRobatjazi <mohamadrobatjazi1387@gmail.com>
2026-08-03 21:20:48 +0330

    feat: add layout

--- src/views/AppView.vue
+++ src/views/AppView.vue
@@ -1,5 +1,42 @@
 <template>
-    <RouterView />
+    <SidebarProvider>
+        <AppSidebar />
+
+        <SidebarInset>
+            <header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                <SidebarTrigger class="-ms-2" />
+                <Separator orientation="vertical" class="me-2 h-4" />
+
+                <Breadcrumb>
+                    <BreadcrumbList>
+                        <BreadcrumbItem class="hidden md:block">
+                            <BreadcrumbLink href="#"> Building Your Application </BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator class="hidden md:block" />
+                        <BreadcrumbItem>
+                            <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                        </BreadcrumbItem>
+                    </BreadcrumbList>
+                </Breadcrumb>
+            </header>
+
+            <div class="flex flex-1 flex-col gap-4 p-4">
+                <RouterView />
+            </div>
+        </SidebarInset>
+    </SidebarProvider>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import AppSidebar from '@/components/AppSidebar.vue';
+    import { Separator } from '@/components/ui/separator';
+    import { SidebarProvider, SidebarInset, SidebarTrigger } from '@/components/ui/sidebar';
+    import {
+        Breadcrumb,
+        BreadcrumbList,
+        BreadcrumbItem,
+        BreadcrumbLink,
+        BreadcrumbPage,
+        BreadcrumbSeparator
+    } from '@/components/ui/breadcrumb';
+</script>
