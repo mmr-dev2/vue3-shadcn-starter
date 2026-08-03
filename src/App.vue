7c98a3b794935d13246a537b268ed8d5f0a07385
MohamadRobatjazi <mohamadrobatjazi1387@gmail.com>
2026-08-03 22:25:47 +0330

    feat: add Toaster provider

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
     <RouterView />
+
+    <Toaster/>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import 'vue-sonner/style.css';
+
+    // Components
+    import { Toaster } from '@/components/ui/sonner';
+</script>
